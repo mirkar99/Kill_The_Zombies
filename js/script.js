6c2cfc1aa7f256e7b43a2d5cc11952c217773f52
Bartosz Kosik <mirkar99@gmail.com>
2023-01-18 16:29:17 +0100

    added levels object, pointsAccumulator, actualLevel variable  and createNextLevelButton function

--- js/script.js
+++ js/script.js
@@ -21,7 +21,31 @@ const shopBackButton = shop.children[1];
 const zombiesInterval = [];
 let gameInterval;
 let killedZombies = 0;
+let pointsAccumulator = 0;
+let actualLevel = 1;
 
+const levels = {
+    1: {
+        background: '../img/Cemetery.png',
+        zombiesToKill: 2,
+        levelWasWon: false
+    },
+    2: {
+        background: '../img/Cemetery.png',
+        zombiesToKill: 4,
+        levelWasWon: false
+    },
+    3: {
+        background: '../img/Cemetery.png',
+        zombiesToKill: 6,
+        levelWasWon: false
+    },
+    4: {
+        background: '../img/Cemetery.png',
+        zombiesToKill: 8,
+        levelWasWon: false
+    }
+}
 const randomElementPositionX = (el) => {
     const viewportWidth = window.visualViewport.width;
     const maxRage = viewportWidth + 1 - el.offsetWidth;
@@ -44,20 +68,39 @@ const scaleElement = (el, scale) => {
 const zombieAttack = function () {
     userHealth.innerText = Number(userHealth.innerText) - 5;
 }
-const zombieNewTop = el => el.style.top = `${window.visualViewport.height*3/5}px`;
+const zombieNewTop = el => el.style.top = `${window.visualViewport.height * 3 / 5}px`;
 
 const createNewGame = () => {
     userPoints.innerText = '0';
     userAmmo.innerText = 14;
     userHealth.innerText = 100;
     menu.classList.add('hidden')
+    game.style.backgroundImage = `url(${levels[actualLevel].background})`;
     game.classList.remove('hidden')
     afterGameMenu.classList.add('hidden')
     gameInterval = setInterval(() => {
         createNewZombie();
     }, 1000)
 }
-
+const createNextLevelButton = () => {
+    if(!document.querySelector('.menu__button--next')){
+    if (actualLevel < Object.keys(levels).length) {
+        if (levels[actualLevel].levelWasWon) {
+            const parent = document.querySelector('.menu--after-game');
+            const btn = document.createElement('button');
+            btn.innerText = 'Next Level';
+            btn.classList.add('menu__button');
+            btn.classList.add('menu__button--next');
+            btn.addEventListener('click', () => {
+                actualLevel++;
+                createNewGame();
+                btn.remove()
+            })
+            parent.append(btn)
+        }
+    }
+}
+}
 const functionalityForNewZombie = (el) => {
     randomElementPositionX(el);
     let scaleValue = 1.0;
@@ -83,7 +126,7 @@ const functionalityForNewZombie = (el) => {
     });
 }
 
-const endGame = (text) => {
+const endGame = (text, winStatus) => {
     clearInterval(gameInterval);
     game.classList.add('hidden');
     const zombies = document.querySelectorAll('.zombie');
@@ -91,6 +134,11 @@ const endGame = (text) => {
     zombiesInterval.forEach(el => clearInterval(el));
     zombiesInterval.splice(0, zombiesInterval.length);
     killedZombies = 0;
+    if (winStatus && levels[actualLevel].levelWasWon == false) {
+        levels[actualLevel].levelWasWon = true;
+        pointsAccumulator += Number(userPoints.innerHTML);
+    }
+    createNextLevelButton();
     afterGameMenuText.innerText = text;
     afterGameMenu.classList.remove('hidden');
 }
@@ -118,8 +166,11 @@ game.addEventListener('click', () => {
         if (Number(userAmmo.innerText) === 0) {
             userAmmo.innerText = 'Click Realod';
         }
-        if (killedZombies === 15) {
-            endGame(`You Won and get ${userPoints.innerText} points`);
+        if (killedZombies === levels[actualLevel].zombiesToKill) {
+            if (levels[actualLevel].levelWasWon == true) {
+                return endGame(`You did Won again`, true);
+            }
+            endGame(`You Won and get ${userPoints.innerText} points`, true);
         }
     }
 });
@@ -133,7 +184,7 @@ userAmmo.addEventListener('click', () => {
 const playerHealthChacker = function (mutation) {
     mutation.forEach(function (mutation) {
         if (Number(userHealth.innerHTML) === 0) {
-            endGame(`You Lose`);
+            endGame(`You Lose`, false);
         }
     });
 }
@@ -145,8 +196,10 @@ observeHealth.observe(userHealth, config);
 
 const saveState = function () {
     const save = {
-        'PointsAmount': userPoints.innerHTML,
-        'afterGameMenuText': afterGameMenuText.innerText
+        'PointsAmount': pointsAccumulator,
+        'afterGameMenuText': afterGameMenuText.innerText,
+        'actualLevel': actualLevel,
+        'levelProgress': levels
     }
     localStorage.setItem('Kill_The_Zombies_Save', JSON.stringify(save));
 }
@@ -155,8 +208,15 @@ const loadState = function () {
         const loadedSave = JSON.parse(localStorage.getItem('Kill_The_Zombies_Save'));
         menu.classList.add('hidden');
         afterGameMenu.classList.remove('hidden');
-        userPoints.innerText = loadedSave.PointsAmount;
+        pointsAccumulator = loadedSave.PointsAmount;
         afterGameMenuText.innerText = loadedSave.afterGameMenuText;
+        actualLevel = loadedSave.actualLevel;
+        Object.keys(levels).forEach(function (key) {
+            if (loadedSave.levelProgress[key]) {
+                levels[key] = loadedSave.levelProgress[key]
+            }
+        });
+        createNextLevelButton();
     }
 }
 loadtButton.addEventListener('click', () => {
@@ -165,6 +225,7 @@ loadtButton.addEventListener('click', () => {
 shopButton.addEventListener('click', () => {
     afterGameMenu.classList.add('hidden');
     shop.classList.remove('hidden');
+    shop.children[0].innerText = `Amount of points: ${pointsAccumulator}`
 })
 shopBackButton.addEventListener('click', () => {
     shop.classList.add('hidden');
